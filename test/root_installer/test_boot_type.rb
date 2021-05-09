fed603320ed7579dabf1c1845afb74884e8b73ab
Human Capitalist <human-capitalist@allaiz.com>
2021-05-09 06:39:33 -0400

    tests working

--- test/root_installer/test_boot_type.rb
+++ test/root_installer/test_boot_type.rb
@@ -1,6 +1,5 @@
 require 'test'
 
-
 class TestBootType < Test
   
   def test_efi_4k
@@ -14,7 +13,7 @@ class TestBootType < Test
             assert_instance_of Question::Dialog, q.efi_advisory_dialog
             assert_equal "Boot Type", q.efi_advisory_dialog.title
             assert_equal "YAROZI - Yet Another Root On ZFS installer", q.efi_advisory_dialog.backtitle
-            assert_equal nil, q.efi_advisory_dialog.ok_label
+            assert_nil   q.efi_advisory_dialog.ok_label
             assert_equal fetch_or_save(result.to_s), result.to_s
           end
         end
@@ -22,29 +21,13 @@ class TestBootType < Test
     end
   end
 
-
-
-  # def test_dialog
-  #   with_disk_sets do |set|
-  #     overview = RootInstaller::Questions::BootType.new(nil)
-  #     result = nil
-  #     with_screen 40, 200 do
-  #       with_dialog :msgbox, Proc.new{|*args| result = args} do
-  #         overview.ask
-  #       end
-  #     end
-  #     assert_equal "Introduction and Disk Overview", overview.dialog.title
-  #     assert_equal "YAROZI - Yet Another Root On ZFS installer", overview.dialog.backtitle
-  #     assert_equal "continue\\ and\\ select\\ disks", overview.dialog.ok_label
-  #     assert_equal fetch_or_save(result.to_s, set), result.to_s
-  #   end
-  # end
-
   def test_ok
     with_disk_sets do |set|
       with_dialog :yesno, true do
-        assert_not_quit do
-          RootInstaller::Questions::BootType.new(nil).ask
+        with_dialog :msgbox, true do
+          assert_not_quit do
+            RootInstaller::Questions::BootType.new(nil).ask
+          end
         end
       end
     end
