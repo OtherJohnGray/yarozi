e12fbd7e91cc23cc74a9ea2622d2a0980f4d9d2f
Human Capitalist <human-capitalist@allaiz.com>
2021-05-15 09:23:06 -0400

    working on encryption

--- test/root_installer/test_encryption.rb
+++ test/root_installer/test_encryption.rb
@@ -6,18 +6,18 @@ class TestEncryption < Test
     mixed_disks do
       result = nil
       with_screen 46, 200 do
-        # with_dialog :menu, Proc.new{|*args| result = args; "ZFS"} do
-          # assert_not_quit do
+        with_dialog :menu, Proc.new{|*args| result = args; "ZFS"} do
+          assert_not_quit do
             q = RootInstaller::Questions::Encryption.new(nil)
             q.ask
-            # assert_instance_of Question::Dialog, q.dialog
-            # assert_equal "Root Dataset Encryption", q.dialog.title
-            # assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
-            # assert_nil   q.dialog.ok_label
-            # assert_equal fetch_or_save(result.to_s), result.to_s
-            # assert_equal "ZFS", q.root_encryption_type
-          # end
-        # end
+            assert_instance_of Question::Dialog, q.dialog
+            assert_equal "Root Dataset Encryption", q.dialog.title
+            assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
+            assert_nil   q.dialog.ok_label
+            assert_equal fetch_or_save(result.to_s), result.to_s
+            assert_equal "ZFS", q.root_encryption_type
+          end
+        end
       end  
     end  
   end
