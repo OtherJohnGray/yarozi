618a5714301f6d70499314428ce338c1d325ef00
Human Capitalist <human-capitalist@allaiz.com>
2021-05-15 04:44:23 -0400

    next questions...

--- lib/root_installer/installer.rb
+++ lib/root_installer/installer.rb
@@ -4,6 +4,10 @@ class RootInstaller::Installer < Task
     questions << ( @continue = RootInstaller::Questions::Continue.new(self) )
     questions << ( @disks = RootInstaller::Questions::DiskOverview.new(self) )
     questions << ( @disks = RootInstaller::Questions::BootType.new(self) )
+    # questions << ( @disks = RootInstaller::Questions::Encryption.new(self) )
+    # questions << ( @disks = RootInstaller::Questions::Swap.new(self) )
+    # questions << ( @disks = RootInstaller::Questions::Partitions.new(self) )
+    # questions << ( @disks = RootInstaller::Questions::InstallDetails.new(self) )
   end
 
   def perform
