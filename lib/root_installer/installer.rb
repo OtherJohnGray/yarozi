ad0eb7c441f4cf3a2ca9004fc2e009de027ebe84
Human Capitalist <human-capitalist@allaiz.com>
2021-05-16 04:11:58 -0400

    swap

--- lib/root_installer/installer.rb
+++ lib/root_installer/installer.rb
@@ -5,7 +5,7 @@ class RootInstaller::Installer < Task
     questions << ( @disks = RootInstaller::Questions::DiskOverview.new(self) )
     questions << ( @disks = RootInstaller::Questions::BootType.new(self) )
     questions << ( @disks = RootInstaller::Questions::Encryption.new(self) )
-    # questions << ( @disks = RootInstaller::Questions::Swap.new(self) )
+    questions << ( @disks = RootInstaller::Questions::Swap.new(self) )
     # questions << ( @disks = RootInstaller::Questions::Partitions.new(self) )
     # questions << ( @disks = RootInstaller::Questions::InstallDetails.new(self) )
   end
---
+++ test/root_installer/test_swap.rb
@@ -0,0 +1,44 @@
+require 'test'
+
+class TestSwap < Test
+
+  def test_yes
+    mixed_disks do
+      result = nil
+      with_screen 46, 200 do
+        with_dialog :yesno, Proc.new{|*args| result = args} do
+          q = RootInstaller::Questions::Swap.new(nil)
+          q.ask
+          assert_instance_of Question::Dialog, q.dialog
+          assert_equal "SWAP", q.dialog.title
+          assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
+          assert_equal %{"Create encrypted LUKS swap"}, q.dialog.yes_label
+          assert_equal %{"Do NOT create swap"}, q.dialog.no_label
+          assert_equal fetch_or_save(result.to_s), result.to_s
+          assert q.configure_swap
+        end
+      end  
+    end  
+  end
+
+  def test_no
+    mixed_disks do
+      result = nil
+      with_screen 46, 200 do
+        with_dialog :yesno, Proc.new{|*args| result = args; false} do
+          q = RootInstaller::Questions::Swap.new(nil)
+          q.ask
+          assert_instance_of Question::Dialog, q.dialog
+          assert_equal "SWAP", q.dialog.title
+          assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
+          assert_equal %{"Create encrypted LUKS swap"}, q.dialog.yes_label
+          assert_equal %{"Do NOT create swap"}, q.dialog.no_label
+          assert_equal fetch_or_save(result.to_s), result.to_s
+          assert !q.configure_swap
+        end
+      end  
+    end  
+  end
+
+end
+  
