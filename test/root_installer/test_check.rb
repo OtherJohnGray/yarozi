bdfac6eda3fa57696dedfa3c4f06b2646036e57e
Human Capitalist <human-capitalist@allaiz.com>
2021-05-22 23:07:26 -0400

    wizard vs dialog

--- test/root_installer/test_check.rb
+++ test/root_installer/test_check.rb
@@ -30,17 +30,19 @@ class TestCheck < Test
     result = nil
     quits = nil
     largesector_disks do
-      with_dialog :msgbox, Proc.new{|*args| result = args} do
-        check = RootInstaller::Questions::Check.new(nil)
-        check.stub :efi_support?, Proc.new{ false } do
-          check.stub :quit, Proc.new{ quits = 1 } do
-            check.ask
-            assert_equal 1, quits
-            assert_instance_of Dialog, check.dialog
-            assert_equal "ERROR - Install environment not booted via UEFI", check.dialog.title
-            assert_equal "YAROZI - Yet Another Root On ZFS installer", check.dialog.backtitle
-            assert_equal "exit\\ without\\ changes", check.dialog.ok_label
-            assert_equal fetch_or_save(result.to_s), result.to_s
+      with_screen 40, 200 do
+        with_dialog :msgbox, Proc.new{|*args| result = args} do
+          check = RootInstaller::Questions::Check.new(nil)
+          check.stub :efi_support?, Proc.new{ false } do
+            check.stub :quit, Proc.new{ quits = 1 } do
+              check.ask
+              assert_equal 1, quits
+              assert_instance_of Dialog, check.dialog
+              assert_equal "ERROR - Install environment not booted via UEFI", check.dialog.title
+              assert_equal "YAROZI - Yet Another Root On ZFS installer", check.dialog.backtitle
+              assert_equal "exit\\ without\\ changes", check.dialog.ok_label
+              assert_equal fetch_or_save(result.to_s), result.to_s
+            end
           end
         end
       end
