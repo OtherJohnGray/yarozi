90f7730d88329064f3436f1caa4409d12b627f47
Human Capitalist <human-capitalist@allaiz.com>
2021-05-25 21:38:48 -0400

    testing

--- test/root_installer/test_swap.rb
+++ test/root_installer/test_swap.rb
@@ -2,45 +2,31 @@ require 'test'
 
 class TestSwap < Test
 
-  def test_yes
+  def test_ask
     mixed_disks do
       result = nil
       task = Task.new
       with_screen 46, 200 do
-        # with_dialog :yesno, Proc.new{|*args| result = args} do
+        with_dialog :menu, Proc.new{|*args| result = args; "none"} do
           q = RootInstaller::Questions::Swap.new(task)
           q.ask
-          # assert_instance_of Question::Dialog, q.dialog
-          # assert_equal "SWAP", q.dialog.title
-          # assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
-          # assert_equal %{"Create encrypted LUKS swap"}, q.dialog.yes_label
-          # assert_equal %{"Do NOT create swap"}, q.dialog.no_label
-          # assert_equal fetch_or_save(result.to_s), result.to_s
-          # assert task.configure_swap
+          assert_instance_of Dialog, q.wizard
+          assert_equal "SWAP", q.wizard.title
+          assert_equal "YAROZI - Yet Another Root On ZFS installer", q.wizard.backtitle
+          assert_equal fetch_or_save(result.to_s), result.to_s
+          assert_equal "none", q.instance_variable_get(:@choice)
         end
-      # end  
+      end  
     end  
   end
 
-  # def test_no
-  #   mixed_disks do
-  #     result = nil
-  #     task = Task.new
-  #     with_screen 46, 200 do
-  #       with_dialog :yesno, Proc.new{|*args| result = args; false} do
-  #         q = RootInstaller::Questions::Swap.new(task)
-  #         q.ask
-  #         assert_instance_of Question::Dialog, q.dialog
-  #         assert_equal "SWAP", q.dialog.title
-  #         assert_equal "YAROZI - Yet Another Root On ZFS installer", q.dialog.backtitle
-  #         assert_equal %{"Create encrypted LUKS swap"}, q.dialog.yes_label
-  #         assert_equal %{"Do NOT create swap"}, q.dialog.no_label
-  #         assert_equal fetch_or_save(result.to_s), result.to_s
-  #         assert !task.configure_swap
-  #       end
-  #     end  
-  #   end  
-  # end
+  def test_respond
+    q = RootInstaller::Questions::Swap.new(Task.new)
+    q.instance_variable_set :@choice, "none"
+    q.respond
+    assert_equal "none", q.task.configure_swap
+    assert_equal 0, q.subquestions.length
+  end
 
 end
   
--- test/test_question.rb
+++ test/test_question.rb
@@ -23,23 +23,65 @@ class TestQuestion < Test
     end
   end
 
-  def test_sizing
+  def test_alert_sizing
     with_dialog :msgbox do
       with_screen 24, 80 do
         d = Question.new(nil).dialog
-        assert_equal ["this is a test dialog", 19, 70], d.msgbox("this is a test dialog",50,150)
+        assert_equal ["this is a test dialog", 19, 70], d.alert("this is a test dialog",50,150)
       end
       with_screen 60, 200 do
         d = Question.new(nil).dialog
-        assert_equal ["this is a test dialog", 50, 150], d.msgbox("this is a test dialog",50,150)
+        assert_equal ["this is a test dialog", 50, 150], d.alert("this is a test dialog",50,150)
       end
       with_screen 24, 80 do
         d = Question.new(nil).dialog
-        assert_equal ["this is a test dialog", 16, 60], d.msgbox("this is a test dialog",0,0,8,20)
+        assert_equal ["this is a test dialog", 16, 60], d.alert("this is a test dialog",0,0,8,20)
       end
       with_screen 24, 80 do
         d = Question.new(nil).dialog
-        assert_equal ["this is a test dialog", 22, 75], d.msgbox("this is a test dialog",22,75,1,1)
+        assert_equal ["this is a test dialog", 22, 75], d.alert("this is a test dialog",22,75,1,1)
+      end
+    end
+  end
+
+  def test_advise_sizing
+    with_dialog :yesno do
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", 19, 70], d.advise("this is a test dialog",50,150)
+      end
+      with_screen 60, 200 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", 50, 150], d.advise("this is a test dialog",50,150)
+      end
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", 16, 60], d.advise("this is a test dialog",0,0,8,20)
+      end
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", 22, 75], d.advise("this is a test dialog",22,75,1,1)
+      end
+    end
+  end
+
+  def test_ask_sizing
+    with_dialog :menu do
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", ["a","b"], 19, 70, 2], d.ask("this is a test dialog", ["a","b"], 50, 150, 2 )
+      end
+      with_screen 60, 200 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", ["a","b"], 50, 150, 2], d.ask("this is a test dialog", ["a","b"], 50, 150, 2 )
+      end
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", ["a", "b"], 16, 60, 2], d.ask("this is a test dialog", ["a","b"], 0, 0, 2, 8, 20 )
+      end
+      with_screen 24, 80 do
+        d = Question.new(nil).dialog
+        assert_equal ["this is a test dialog", ["a", "b"], 22, 75, 2], d.ask("this is a test dialog", ["a","b"], 22, 75, 2, 1, 1 )
       end
     end
   end
