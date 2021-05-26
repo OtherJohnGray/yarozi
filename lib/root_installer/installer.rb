5f824e4cea570b3d823b6350c0d609e9ce9ad7ee
Human Capitalist <human-capitalist@allaiz.com>
2021-05-26 08:29:09 -0400

    formatting

--- lib/root_installer/installer.rb
+++ lib/root_installer/installer.rb
@@ -5,13 +5,16 @@ class RootInstaller::Installer < Task
     questions.append RootInstaller::Questions::Check.new(self)
     questions.append RootInstaller::Questions::Continue.new(self)
     questions.append RootInstaller::Questions::DiskOverview.new(self)
+
     questions.append RootInstaller::Questions::BootType.generate(self)
+    
     questions.append RootInstaller::Questions::Encryption.new(self)
     questions.append RootInstaller::Questions::Swap.new(self)
     # questions.append RootInstaller::Questions::Compression.new(self)
     # questions.append RootInstaller::Questions::Partitions.new(self)
     # questions.append RootInstaller::Questions::InstallDetails.new(self)
   end
+
 
   def perform
     #p Disk.all
