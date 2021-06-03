0302ac2557e4c0a669fc67cc919ad01506ba361b
Human Capitalist <human-capitalist@allaiz.com>
2021-06-03 07:49:49 -0400

    test yvn

--- lib/y_v_n.rb
+++ lib/y_v_n.rb
@@ -1,27 +1,25 @@
 class YVN
+    attr_accessor :yvn_string, :swap, :errors
 
-    def initialize(yvn_string)
+    def initialize(yvn_string, swap=false)
       @yvn_string = yvn_string
+      @swap = swap
       if @yvn_string && @vyvn_string.length > 0
         @vdev_strings = @yvn_string.gsub(/\s+/m, ' ').strip.split(" ")
         @vdevs = @vdev_string.map {|vdev_string| VDEV.new vdev_string }
+        @errors = @vdevs.map{|v| v.errors}.flatten
       else
-        @errors << "yvn_string was empty"
+        @errors ["yvn_string was empty"]
       end
     end
 
-
-    def valid_zpool?
-      @vdevs && @vdevs.all? {|v| v.valid_vdev?}
+    def valid?
+      @errors.empty?
     end
 
-    def valid_swap?
-      @vdevs && @vdevs.all? {|v| v.valid_swap?}
-    end
-
-    def zpool_errors?
 
     class VDEV
+      attr_accessor :vdev_string, :type, :size, :units
 
       TYPE_PATTERN  = /^(S|M|Z1|Z2|Z3|R1|R5|R6)/
       SIZE_PATTERN  = /(\d+)/
@@ -41,11 +39,16 @@ class YVN
           @size  = m[2]
           @units = m[3]
           @disks = m[4]
+
+UP TO HERE - NEED TO ALLOW MULTIPLE RANGES MIXED WITH NUMBERS, and disambiguate @disks from @disks_string
+
           if r = RANGE_PATTERN.match(@disks)
             parse_range_match r
           else
             parse_list_match LIST_PATTERN.match(@disks)
           end
+          @errors << "must start with S, M, Z1, Z2, or Z3 for ZFS" unless swap || %w{ S, M, Z1, Z2, Z3 }.include?(@type)
+          @errors << "must start with S, R1, R5, or R6 for Swap" if swap && !%w{ S, R1, R5, R6 }.include?(@type)
         else
           @errors << "must start with S, M, Z1, Z2, or Z3 for a ZFS VDEV, or S, R1, R5, or R6 for Swap" unless TYPE_PATTERN =~ vdev_string
           @errors << "must have : as the 2nd or 3rd character" unless [1,2].include?(v.index(":"))
@@ -57,7 +60,7 @@ class YVN
     end
 
     def parse_range_match(match_data)
-      first, last = match_data[1..-1]
+      first, last = match_data[1..-1].map{|m| m.to_i }
       if first < 0
         @errors << "first disk number of #{first} was less than 1"
       elsif first > Disk.count
@@ -87,18 +90,6 @@ class YVN
         end
         
       end
-    end
-
-    def valid?
-      @errors.empty?
-    end
-
-    def valid_zpool?
-      valid? && %w{ S, M, Z1, Z2, Z3 }.include?(@type)
-    end
-
-    def valid_swap? 
-      valid? && %w{ S, R1, R5, R6 }.include?(@type)
     end
 
 
