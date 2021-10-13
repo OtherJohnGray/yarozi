238ceafd133280eb56daa1efe1266b8f95e29cf8
Human Capitalist <human-capitalist@allaiz.com>
2021-10-13 13:20:48 +1100

    comments

--- lib/z_pool.rb
+++ lib/z_pool.rb
@@ -1,3 +1,11 @@
+# Helper class used by YVN class.
+# Holds a list of VDEV definitions that result from parsing a YVN string.
+#
+# If you are looking for the class that actually holds the ZPool and VDEV
+# information that will be used to build the system, look at layout.rb.
+# Layout is used from within root_installer/questions/partitions.rb to 
+# store generated VDEV and Pool specifications, and provides validation
+# of the layout of VDEVs across the various disks of the system.
 class ZPool < Array
 
   def valid?
